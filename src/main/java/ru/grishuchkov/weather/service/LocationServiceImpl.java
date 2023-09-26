758b37a95c9ad01f378ba54a460bf1b821a01774
grishuchkov <gri.d.a@ya.ru>
2023-09-26 19:53:51 +0300

    refactor: remove all http-client logics to WeatherClient

--- src/main/java/ru/grishuchkov/weather/service/LocationServiceImpl.java
+++ src/main/java/ru/grishuchkov/weather/service/LocationServiceImpl.java
@@ -4,17 +4,14 @@ package ru.grishuchkov.weather.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.grishuchkov.weather.entity.Location;
+import ru.grishuchkov.weather.entity.User;
+import ru.grishuchkov.weather.repo.ifc.LocationRepository;
 import ru.grishuchkov.weather.service.ifc.LocationService;
+import ru.grishuchkov.weather.service.ifc.WeatherApiClient;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 
@@ -22,47 +19,38 @@ import java.util.List;
 public class LocationServiceImpl implements LocationService {
 
     private final JsonMapper jsonMapper;
+    private final WeatherApiClient weatherApiClient;
+
+    private final LocationRepository locationRepository;
 
     @Autowired
-    public LocationServiceImpl(JsonMapper jsonMapper) {
+    public LocationServiceImpl(JsonMapper jsonMapper, WeatherApiClient weatherApiClient, LocationRepository locationRepository) {
         this.jsonMapper = jsonMapper;
+        this.weatherApiClient = weatherApiClient;
+        this.locationRepository = locationRepository;
     }
 
-    @Value("${openweather.token}")
-    private String API_KEY;
 
     @Override
-    public List<Location> getLocationsByName(String name) throws URISyntaxException, IOException, InterruptedException {
-        //todo: weather API Client class --impl--> OpenWeatherApiClient : getLocationsByName and getWeatherByCoordinates;
+    public List<Location> getLocationsByName(String name) throws IOException {
 
-        name = name.replaceAll("\\s","-");
+        name = prepareLocationName(name);
 
-        final String BASE_API_URL = "http://api.openweathermap.org/geo/1.0/direct";
-
-        String urlencoded = UriComponentsBuilder.fromHttpUrl(BASE_API_URL)
-                .queryParam("q", name)
-                .queryParam("limit", 10)
-                .queryParam("appid", API_KEY)
-                .queryParam("units", "metric")
-                .encode()
-                .toUriString();
-
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(new URI(urlencoded))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-
+        HttpResponse<String> response = weatherApiClient.findLocationsByName(name);
         List<Location> resultList = jsonMapper.readValue(response.body(), new TypeReference<>() {});
 
         return resultList;
     }
 
-    @Override
-    public void setNewLocation(Location location) {
 
+    @Override
+    public void saveNewLocationFromUser(Location location, String username) {
+
+        locationRepository.saveNewLocationByUser(location, new User(username));
     }
 
+    private String prepareLocationName(String name) {
+        name = name.replaceAll("\\s","-");
+        return name;
+    }
 }
